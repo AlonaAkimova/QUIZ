c2e94a1dc12999526d8e8db45caa92372b76e841
AlonaAkimova <aliona.akimova@gmail.com>
2023-11-10 11:43:33 +0700

    update

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+export default function QuestionTimer({ timeout, onTimeout }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <progress id="question-timer" max={timeout} value={remainingTime} />;
+}
